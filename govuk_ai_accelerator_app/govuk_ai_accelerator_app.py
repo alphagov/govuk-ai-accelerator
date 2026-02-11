8c9ec59622e8a9acfefdacbbe87336e6e9df0551
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2026-02-11 10:46:15 +0000

    Added a waitress python http server so flash doesn't need to run in dev mode in the container

--- govuk_ai_accelerator_app/govuk_ai_accelerator_app.py
+++ govuk_ai_accelerator_app/govuk_ai_accelerator_app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask import Blueprint
 
-app = Flask(__name__)
 
-@app.route("/")
+test = Blueprint('admin', __name__, url_prefix='/test')
+
+@test.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(test)
+
+    return app
+
 if __name__ == '__main__':  
-    app.run(host='0.0.0.0', port=5000) 
+    create_app().run(host='0.0.0.0', port=8080) 
