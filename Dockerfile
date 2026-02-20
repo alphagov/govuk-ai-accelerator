f33c729c11d84e73aa37b78a38b56ca42d931e6a
daymohlar <ademola.adefioye@thoughtworks.com>
2026-02-20 13:05:10 +0000

    Testing Demo background workers (#10)
    * adding demo background workers
    
    * adding accelerator from git
    
    * Modifying git and docker ignore files to properly capture venv files
    
    * Adding more testing functions
    * Bedrock
    * AWS S3 connection
    
    * temp removing accelerator
    
    * adding the right environment name
    
    * Making modification to the github workflow to include arguements needed for dockerbuild
    
    * Bug fix
    
    ---------
    
    Co-authored-by: Ademola Adefioye <ademolaadefioye@Ademolas-MacBook-Pro.local>

--- Dockerfile
+++ Dockerfile
@@ -2,22 +2,35 @@
 FROM python:3.13-slim-bookworm AS base
 
 ENV GOVUK_APP_NAME=GOVUK-AI-ACCELERATOR
+ARG GOVUK_CI_GITHUB_API_TOKEN
+
+
 
 RUN apt-get update \
     && apt-get install -y --no-install-recommends \
     libcurl4 \
     curl \
     postgresql-client \
+    git \
     && apt-get -y clean && \
     rm -rf /var/lib/apt/lists/* /tmp/*
 
 WORKDIR /app  
-COPY requirements.txt .
 
-RUN echo "Installing python requirements" && \
-    pip --no-cache-dir install -r requirements.txt
+RUN pip install --no-cache-dir uv
+RUN uv init
+
+COPY requirements.txt .
+RUN uv pip install --system -r requirements.txt
+RUN uv pip install --system "git+https://${GOVUK_CI_GITHUB_API_TOKEN}@github.com/alphagov/govuk-ai-accelerator-tw-accelerator"
+
 
 COPY . .
+COPY ./environment.sh /environment.sh
+RUN chmod +x /environment.sh
+
 EXPOSE 8080 
 
-CMD ["waitress-serve", "--port", "3000", "--call", "govuk_ai_accelerator_app:create_app"]  
+ENTRYPOINT ["/bin/bash", "-c", "source /environment.sh && \"$@\"", "--"]
+
+CMD ["uv", "run", "waitress-serve", "--host=0.0.0.0", "--port=8080", "--call", "govuk_ai_accelerator_app:create_app"]
--- govuk_ai_accelerator_app.py
+++ govuk_ai_accelerator_app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask import Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 import os
+from scripts.pipeline.worker import run_counter, llm_fact, list_s3_directories
+from concurrent.futures import ThreadPoolExecutor
+ # Usage
+
+executor = ThreadPoolExecutor(max_workers=4)
 
 root = Blueprint('test', __name__, url_prefix='/')
 healthcheck = Blueprint('healthcheck', __name__, url_prefix='/healthcheck')
+worker = Blueprint('worker', __name__, url_prefix='/worker')
 
 db = SQLAlchemy()
 
@@ -19,6 +25,56 @@ def db_test():
     test = db.first_or_404(db.select(Test))
     return "<p>Hello, World! " + test.info + "</p>" 
 
+
+@worker.route("/test")
+def counter():
+    raw_val = request.args.get('no')
+
+    if raw_val is not None and raw_val.isdigit():
+        number = int(raw_val)
+        executor.submit(run_counter, number)
+    
+        return {"status": "background work kicked off"}
+    
+from flask import request, jsonify
+
+
+
+@worker.route("/list")
+def s3_check():
+    bucket = request.args.get('bucket')
+    prefix = request.args.get('prefix', '')
+
+    if not bucket:
+        return jsonify({"error": "Bucket name is required"}), 400
+
+    try:
+        response  = list_s3_directories(bucket, prefix)
+        directories = [
+            item['Prefix'] for item in response.get('CommonPrefixes', [])
+        ]
+
+        return jsonify({
+            "status": "success",
+            "bucket": bucket,
+            "directories": directories
+        })
+        
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+
+
+
+@worker.route("/llm")        
+def space_greetings():
+    response = llm_fact()
+
+    return jsonify({
+        "funfact": response
+    })
+
+
+
 @healthcheck.route("/ready")
 def ready():
     return "<p>Roger, Roger! Application is ready."
@@ -27,6 +83,7 @@ def create_app():
     app = Flask(__name__)
     app.register_blueprint(root)
     app.register_blueprint(healthcheck)
+    app.register_blueprint(worker)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     db.init_app(app)
