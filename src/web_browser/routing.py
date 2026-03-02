7e65bfaea0e3126e75922fabf07e2c667f623676
Kev Bolam <kevbolam@Kevs-MacBook-Pro.local>
2026-03-02 11:24:19 +0000

    change made to s3 config

--- src/web_browser/routing.py
+++ src/web_browser/routing.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 def index(app) -> str:
     app = Flask(__name__)
     if "AWS_KWARGS" in app.config:
-        s3 = boto3.resource("s3", app.config["AWS_KWARGS"])
+        s3 = boto3.resource("s3")
         all_buckets = s3.buckets.all()
         return render_template("index.html", buckets=all_buckets)
     else:
