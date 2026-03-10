672d38e3d996412463ab5aacd079da7328c779e9
John Mikel Amiel Regida <john.regida@digital.cabinet-office.gov.uk>
2026-03-10 10:49:23 +0000

    fix: move imports inside runtime code for accelerator library

--- scripts/pipeline/logging_config.py
+++ scripts/pipeline/logging_config.py
@@ -1,6 +1,16 @@
-from typing import cast
-from taxonomy_ontology_accelerator.commons.utils.logger import get_logger
+import logging
+from typing import Any, cast
 
-logger = cast('RichLogger', get_logger())
+try:
+    from taxonomy_ontology_accelerator.commons.utils.logger import get_logger
+except ModuleNotFoundError:
+    def get_logger() -> logging.Logger:
+        logger = logging.getLogger("govuk-ai-accelerator")
+        if not logger.handlers:
+            logging.basicConfig(level=logging.INFO)
+        return logger
+
+
+logger = cast(Any, get_logger())
 
 __all__ = ['logger']
